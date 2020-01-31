525fea09e142cb24a6dd78b0a87cd3b492f8b431
federicodeservi <56127470+federicodeservi@users.noreply.github.com>
2020-01-31 09:09:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     name='mtsb',
     license="MIT",
     description='Python library that collects tweets about movies, performs a sentiment analysis and correlates it with the boxoffice result of the 7 days after the movie release.',
-    version='v0.0.6',
+    version='v0.0.7',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/federicodeservi/mtsb',
